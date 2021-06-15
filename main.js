6a4621e04e6080994eac0b89f0e32d8c268fb264
Viktorodessa <85935107+Viktorodessa@users.noreply.github.com>
2021-06-15 14:37:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('.socials__item_Mod').click(function () {
+		$('#modal').addClass('show');
+	})
+});
+$('.modal2__overlay').click(function () {
+	    $('#modal').removeClass('show')	
+});
